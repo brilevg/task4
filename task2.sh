f3a81b3f37316d9f1d2230e52dc25e6bf960d6b3
unknown <brilevlad@gmail.com>
2026-05-23 18:25:33 +1000

    14

--- task2.sh
+++ task2.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
 
 HOST_UUID=$(cat /proc/sys/kernel/random/uuid)
-HOSTNAME=$(hostname)
+
 OS_NAME=$(uname -o)
+
 KERNEL=$(uname -r)
 
 OS_ID=$(docker exec -i postgres psql -U monitor -d monitoring -t -c "
 INSERT INTO os_info
 (host_uuid, hostname, os_name, kernel_version)
 VALUES
-('$HOST_UUID', '$HOSTNAME', '$OS_NAME', '$KERNEL')
+('$HOST_UUID', 'archlinux', '$OS_NAME', '$KERNEL')
 RETURNING id;
 " | tr -d ' ')
 
-jq -c '.components[]' /home/arch/sbom/sbom.json | while read i
+grep '"name"' /home/arch/sbom/sbom.json > /tmp/names.txt
+grep '"version"' /home/arch/sbom/sbom.json > /tmp/versions.txt
+
+paste /tmp/names.txt /tmp/versions.txt | while read line
 do
-    NAME=$(echo $i | jq -r '.name')
-    VERSION=$(echo $i | jq -r '.version')
+    NAME=$(echo "$line" | cut -d '"' -f4)
+
+    VERSION=$(echo "$line" | rev | cut -d '"' -f2 | rev)
 
     docker exec -i postgres psql -U monitor -d monitoring -c "
     INSERT INTO software
